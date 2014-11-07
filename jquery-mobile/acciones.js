bb0c4407b3dff1bad225f1f04e3c5bd8b9febd09
alumno355ecb16 <dc99es@gmail.com>
2014-11-07 09:27:36 -0600

    3subida

--- jquery-mobile/acciones.js
+++ jquery-mobile/acciones.js
@@ -6,20 +6,20 @@ $(document).ready(function(e) {
 		$('#disp table td').eq(7).text(device.platform);
 		$('#disp table td').eq(9).text(device.version);
 		$('#disp table td').eq(11).text(device.uuid);
-   document.addEventListener("pause",function() {
+   document.addEventListener("pause",function(){
 	   escribehistoria('la app se pauso');
    },false);//pause
-   document.addEventListener("resume",function() {
+   document.addEventListener("resume",function(){
 	   escribehistoria('la app se reinicio');
    },false);//resume
-   document.addEventListener("online",function() {
+   document.addEventListener("online",function(){
 	   escribehistoria('la app se conecto a la red');
    },false);//se conecto
-   document.addEventListener("offline",function() {
+   document.addEventListener("offline",function(){
 	   escribehistoria('la app se desconecto de la red');
    },false);//se desconecto
    },false);//ready device
-},false);//document
-function escribehistoria(accion) {
+});//document
+function escribehistoria(accion){
 	$('#eHistoria').append('<li>'+accion+'</li>');
 }
